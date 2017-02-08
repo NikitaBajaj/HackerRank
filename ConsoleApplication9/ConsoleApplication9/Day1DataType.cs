c4339ae00aae98c17bfe19f531fa84c7034b2725
NikitaBajaj <nikitabajaj9222@gmail.com>
2017-02-08 14:05:35 +0530

    Add files via upload

---
+++ ConsoleApplication9/ConsoleApplication9/Day1DataType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Day1DataType
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "Hacker Rannk !";
+            // Declare second integer, double, and String variables.
+            i += Convert.ToInt32(Console.ReadLine());
+            d += Convert.ToDouble(Console.ReadLine());
+            s += Console.ReadLine();
+            Console.WriteLine(i);
+            Console.WriteLine(d.ToString("F1"));
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}
